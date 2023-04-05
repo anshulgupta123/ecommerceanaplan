a56c212ef691d8e1b44536e58356cd68d583a858
anshul.kumar2 <anshul.kumar2@globallogic.com>
2023-04-05 15:44:56 +0530

    commited for api gateway

---
+++ cloudgatewayecommerce/src/test/java/com/example/cloudgatewayecommerce/CloudgatewayecommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cloudgatewayecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudgatewayecommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
